6546d89b15ec37abbe3c64dfadf55fda6b38ab3a
Russell Stephens <rstephens@tonicdesign.com>
2015-12-20 13:25:38 -0500

    wiring up question textview

--- app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
@@ -28,7 +28,7 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +49,10 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 
     @Override
