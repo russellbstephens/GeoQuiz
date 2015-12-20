c58ece27d37409ac179e9a39b12d6151cd59f0ae
Russell Stephens <rstephens@tonicdesign.com>
2015-12-20 13:16:37 -0500

    adding next button; adding question array

--- app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
@@ -9,13 +9,26 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
 
+    private Question[] mQuestionBank = new Question[] {
+        new Question(R.string.question_oceans, true),
+        new Question(R.string.question_midwest, false),
+        new Question(R.string.question_africa, false),
+        new Question(R.string.question_americas, true),
+        new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
