82412b6bc9f6847e6ea57d32985a1ff3192390cd
Russell Stephens <rstephens@tonicdesign.com>
2015-12-20 13:42:11 -0500

    wiring up logic

--- app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/russell/geoquiz/QuizActivity.java
@@ -57,7 +57,7 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -65,7 +65,7 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
